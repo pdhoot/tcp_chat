5834f12b844894ed48bebab22bf64275feb34aa0
Punit Dhoot <punitdhoot1@gmail.com>
2015-08-27 14:53:21 +0530

    Made it possible to shutdown safely via ~q command

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket , sys , signal
 from Node import Node
+from threading import Thread
+from clint.textui import colored
 
 class Client(Node):
 
@@ -10,16 +12,34 @@ class Client(Node):
 
 	def start(self , username):
 		self.sock.connect((self.host , self.port))
-		def signal_handler(signal , frame):
-			print "Ok! The client wants to shut down!"
-			self.sock.close()
-			sys.exit(0)
-		while True:
-			msg = raw_input()
-			signal.signal(signal.SIGINT , signal_handler)
+		Thread(target=self.sends , args = (username,)).start()
+		Thread(target=self.recvs , args=('\n',)).start()
+
+	def sends(self , username):
+		while True: 
+			msg = raw_input("<me>")
+			# signal.signal(signal.SIGINT , signal_handler)
+			if msg=='~q':
+				msg+='\n'
+				self.sendMsg(self.sock , msg)
+				self.sock.shutdown(socket.SHUT_WR)
+				break
 			msg+='\n'
 			msg = '<' + username + '>' + msg
 			self.sendMsg(self.sock , msg)
+	def recvs(self , delimeter):
+		while True:
+			msg = self.recvMsg(self.sock , delimeter)
+			if msg.replace('\n' , '')=='~q':
+				# self.sock.shutdown(socket.SHUT_RD)
+				self.sock.close()
+				break
+			print msg.replace('\n' , '')
+
+def signal_handler(signal , frame):
+	print "Ok! The client wants to shut down!"
+	self.sock.close()
+	sys.exit(0)
 
 
 if __name__=='__main__':
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-import socket, sys
+import socket, sys , signal
 from threading import Thread
 from Node import Node
+
 
 class Server(Node):
 
@@ -9,6 +10,7 @@ class Server(Node):
 		self.port = port
 		self.sock = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
 		self.sock.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1)
+		self.registered_clients = []
 
 	def start(self):
 		self.sock.bind((self.host , self.port))
@@ -18,19 +20,38 @@ class Server(Node):
 				message = self.recvMsg(sc , '\n')
 				if not message:
 					break
+				if message.replace('\n' , '')=='~q':
+					self.sendMsg(sc , message)
+					break
 				print message.replace('\n' , '')
+				self.sendToAllClients(message , sc)
 				#Later will have to add a method to escape \n in case it is already present in the message.
+			self.deregisterClient(sc)
 			sc.close()
 		while True:
 			sc , peeraddr = self.sock.accept()
+			self.registerClient(sc)
 			Thread(target=chat , args=(sc,)).start()
 
-	def registerClient(self):
-		print "Hello"
-		#This is basically to register all the clients
-		#that will be receiving any message sent to the server
-		#basically these are the clients registered on the server
+	def registerClient(self , sc):
+		if sc not in self.registered_clients:
+			self.registered_clients.append(sc)
+
+	def sendToAllClients(self , msg , sc):
+		for sock in self.registered_clients:
+			if sock is not sc:
+				sock.sendall(msg)
+
+	def deregisterClient(self , sc):
+		if sc in self.registered_clients:
+			self.registered_clients.remove(sc)
+
+def signal_handler(signal , frame):
+	print "Ok! The client wants to shut down!"
+	self.sock.close()
+	sys.exit(0)
 
 if __name__=='__main__':
 	server = Server(sys.argv[1] , 1060)
 	server.start()
+	signal.signal(signal.SIGINT , signal_handler)
